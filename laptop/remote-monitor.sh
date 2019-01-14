1e63051c27bb2857f0456c9e9809c1ff35a27f3e
Abby Embree <spacerainbow000@gmail.com>
2019-01-14 14:27:17 -0800

    improved monitor ID detection

--- laptop/remote-monitor.sh
+++ laptop/remote-monitor.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/bin/bash -x
 
-#idk what in this list actually needs to be here
 export DISPLAY=:0
 export COLORTERM=truecolor
 export XDG_VTNR=2
@@ -16,9 +15,10 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/400003190/bus
 export XDG_RUNTIME_DIR=/run/user/400003190
 export XAUTHORITY=/run/user/400003190/gdm/Xauthority
 export XDG_CONFIG_DIRS=/etc/xdg/xdg-dwm:/etc/xdg
+export PATH=/home/aembree/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
 readonly INTERN="eDP-1"
-readonly EXTERN="DP-1-1"
+#readonly EXTERN="DP-1-1"
 
 extern_on=0
 
@@ -26,26 +26,41 @@ echo "\"can't open display\" errors seem to happen from polling xrandr, trying t
 
 while :;
 do
-    if xrandr | grep "${EXTERN} connected" > /dev/null 2>&1 ;
+    if [ $(xrandr | grep " connected" | wc -l) -ge 2 ] > /dev/null 2>&1 ;
     then
-	if [[ ${extern_on} == 0 ]] ;
-	then
-            xrandr --output DP-1-1 --mode 1920x1080
-	    ret=${?}
-	    if [[ ${ret} == 0 ]] ;
-	    then
-	        extern_on=1
-	    else
-	        echo 'error configuring external monitor, trying again...'
-	    fi
-	fi
-    elif xrandr | grep "${EXTERN} disconnected" > /dev/null 2>&1 ;
+        if [[ ${extern_on} == 0 ]] ;
+        then
+            #get monitor ID
+            for f in $(
+                          xrandr | grep ' connected' | grep -v "${INTERN}" | awk '{print $1;}'
+                      ) ;
+            do
+                #debug
+                echo "DEBUG ${f}"
+                if grep -q '1920x1080' <(grep -A1 "${f}" <(xrandr)) ;
+                then
+                    echo "DEBUG setting extern ${f}"
+                    #readonly EXTERN="${f}"
+                    xrandr --output ${f} --mode 1920x1080
+                    ret=${?}
+                    if [[ ${ret} == 0 ]] ;
+                    then
+                        extern_on=1
+                    else
+                        echo 'error configuring external monitor, trying again...'
+                    fi
+                    break
+                fi
+            done
+
+        fi
+    elif [ ! -z ${EXTERN} ] && xrandr | grep "${EXTERN} disconnected" > /dev/null 2>&1 ;
     then
-	if [[ ${extern_on} == 1 ]] ;
-	then
-	    xrandr --output DP-1-1 --off
-	    extern_on=0
-	fi
+        if [[ ${extern_on} == 1 ]] ;
+        then
+            xrandr --output DP-1-1 --off
+            extern_on=0
+        fi
     fi
     sleep 1
 done
